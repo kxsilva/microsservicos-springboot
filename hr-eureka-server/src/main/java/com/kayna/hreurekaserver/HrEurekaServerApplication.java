0ca61c4365eb2473774757855b9e09b9c96884cb
Kaynã Xavier <kayna.xavier@ufpe.br>
2023-04-10 15:45:11 -0300

    Configuring Eureka Server

--- hr-eureka-server/src/main/java/com/kayna/hreurekaserver/HrEurekaServerApplication.java
+++ hr-eureka-server/src/main/java/com/kayna/hreurekaserver/HrEurekaServerApplication.java
@@ -1,8 +1,10 @@
-package com.kayna.hrpayroll;
+package com.kayna.hreurekaserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
 @SpringBootApplication
 public class HrEurekaServerApplication {
 
--- hr-eureka-server/src/test/java/com/kayna/hreurekaserver/HrEurekaServerApplicationTests.java
+++ hr-eureka-server/src/test/java/com/kayna/hreurekaserver/HrEurekaServerApplicationTests.java
@@ -1,4 +1,4 @@
-package com.kayna.hrpayroll;
+package com.kayna.hreurekaserver;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
